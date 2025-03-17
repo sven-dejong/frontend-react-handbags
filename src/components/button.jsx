f9fecf73bfef61ac9f1b158b5a6a445bcd6faeff
sven-dejong <sven_dejong@hotmail.com>
2025-03-17 16:39:50 +0100

    uitwerkingen v1

---
+++ src/components/button.jsx
@@ -0,0 +1,12 @@
+function Button({text, disabled}) {
+    return (
+        <button
+            onClick={() => console.log(text)}
+            disabled={disabled}
+        >
+            {text}
+        </button>
+    )
+}
+
+export default Button;
